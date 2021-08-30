8e32a1ba109423f6b9fd24afbeb69b10b112216f
kaye garcia <kayemgarcia@gmail.com>
2021-08-30 16:47:55 -0500

    commit tree e1635e2b61d4aef1f8a7808651745feb52e8735e

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(freeSample){
+   if(freeSample <= 400){
+     return 'This one is on me!'
+   } else if(freeSample > 2000 && freeSample < 2500){
+     return 'I will gladly take your thirty bucks.'
+   } else (freeSample > 2500); {
+     return 'No can do.'
+   }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+     return city === 'NYC'?'Ok, sounds good.' : 'No go.' 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generousTip){
+  switch(generousTip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+  }
 }
